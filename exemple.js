bf222c762e24add99f61501d8905703e223fbd84
Romain-Constant <romain.constant59@gmail.com>
2023-03-07 13:42:39 +0100

    added some features

--- exemple.js
+++ exemple.js
@@ -1,22 +1,51 @@
 
-const rightPrice = Math.floor(Math.random() * 100) + 1;
 
-let userName = prompt("Quelle est votre nom ?");
-let userChoice = prompt("Choisissez un nombre entre un et 100");
 
-function justePrix (goodPrice, userPrice) {
-    if (userPrice < goodPrice) {
-        userChoice = prompt("C'est plus, quelles est votre nouveau choix?");
-        return justePrix(rightPrice, userChoice);
-    }
-    else if (userPrice > goodPrice) {
-        userChoice = prompt("C'est moins, quelles est votre nouveau choix?");
-        return justePrix(rightPrice, userChoice);
-    }
-    else {
-        return alert(`Bravo, ${userName} gagne !`)
+function startGame () {
+
+    function justePrix (goodPrice, userPrice) {
+        if (userPrice < goodPrice) {
+            count++
+            userChoice = prompt("C'est plus, quel est votre nouveau choix?");
+            return justePrix(rightPrice, userChoice);
+        }
+        else if (userPrice > goodPrice) {
+            count++
+            userChoice = prompt("C'est moins, quel est votre nouveau choix?");
+            return justePrix(rightPrice, userChoice);
+        }
+        else {
+            count++
+            alert(`Bravo, ${userName} gagne en ${count} essai(s) !`);
+            return replay();
+        }
+
+    } 
+
+    function replay () {
+        let userReplay = prompt("Voulez vous rejouer ? (oui / non)"); 
+        if (userReplay === "oui") {
+            return startGame();        
+        }
+        else if (userReplay === "non") {
+            return alert("Dommage, tu aurais pu essayer de faire mieux");
+        }
+        else {
+            alert("Choix non valide. ");
+            return replay();
+        }
     }
 
+    const rightPrice = Math.floor(Math.random() * 100) + 1;
+    let userName = prompt("Quelle est votre nom ?");
+    let userChoice = prompt("Choisissez un nombre entre un et 100");
+    let count = 0;
+
+    justePrix(rightPrice, userChoice);
 }
 
-justePrix(rightPrice, userChoice);
+startGame();
+
+
+
+
